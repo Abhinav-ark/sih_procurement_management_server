e5bc476cab3e5ef92dd17636f9e54c0a9978e95b
Abhinav-ark <abhinavramki2@gmail.com>
2023-10-04 01:02:24 +0530

    getAllProcurements API Test Implementation

--- controller/userWebController.js
+++ controller/userWebController.js
@@ -62,7 +62,65 @@ module.exports = {
             await db_connection.query(`UNLOCK TABLES`);
             db_connection.release();
         }
-    }
+    },
+
+    getAllProcurements: [
+        webTokenValidator,
+        async (req, res) => {
+            if(req.body.userEmail === null || req.body.userEmail === undefined || req.body.userEmail === "" || !validator.isEmail(req.body.userEmail) ||
+            req.body.userRole === null || req.body.userRole === undefined || req.body.userRole === "" ||
+            req.authorization_tier === null || req.authorization_tier === undefined || req.authorization_tier === "" ||
+            (req.authorization_tier!="0" && req.authorization_tier!="1" && req.authorization_tier!="2" && req.authorization_tier!="3" && req.authorization_tier!="4")){
+                return res.status(400).send({ "message": "Access Restricted!" });
+            }
+
+            let db_connection = await db.promise().getConnection();
+
+
+            try {
+
+                if(req.authorization_tier!=="4"){
+                    await db_connection.query(`LOCK TABLES Procurement READ`);
+
+                    let [procurements] = await db_connection.query(`SELECT * from Procurement`);
+
+                    await db_connection.query(`UNLOCK TABLES`);
+
+                    return res.status(200).send({
+                        "message": "All Procurements fetched successfully!",
+                        "procurements": procurements
+                    });
+                }else{
+                    await db_connection.query(`LOCK TABLES Procurement READ, Vendor READ`);
+
+                    let [vendor] =await db_connection.query(`SELECT vendor_ID from Vendor WHERE vendor_Email = ?`, [req.body.userEmail]);
+                    //console.log(id);
+
+                    let [procurements] = await db_connection.query(`SELECT * from Procurement WHERE vendor_ID = ?`, [vendor[0].vendor_ID]);
+                    //console.log(procurements);
+
+                    await db_connection.query(`UNLOCK TABLES`);
+
+                    if(procurements.length===0){
+                        return res.status(400).send({ "message": "No Procurements found!" });
+                    }
+
+                    return res.status(200).send({
+                        "message": "Vendor Procurements fetched successfully!",
+                        "procurements": procurements
+                    });
+                }
+            } catch (err) {
+                console.log(err);
+                const time = new Date();
+                fs.appendFileSync('logs/errorLogs.txt', `${time.toISOString()} - getAllProcurements - ${err}\n`);
+                return res.status(500).send({ "message": "Internal Server Error." });
+            } finally {
+                await db_connection.query(`UNLOCK TABLES`);
+                db_connection.release();
+            }
+        }
+    ]
 
     
 }
--- routes/userWeb.js
+++ routes/userWeb.js
@@ -5,5 +5,6 @@ const userWebController = require('../controller/userWebController');
 router.get('/test', userWebController.test);
 
 router.post('/login', userWebController.userLogin);
+router.get('/getAllProcurements', userWebController.getAllProcurements);
 
 module.exports = router;
