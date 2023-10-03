b59ccc43aacc1f20e1769cfcfdba7ae1d17aeafb
Abhinav R <76396917+Abhinav-ark@users.noreply.github.com>
2023-10-03 23:31:45 +0530

    Update connection.js

--- connection.js
+++ connection.js
@@ -5,11 +5,13 @@ const connectionLimit = os.cpus().length;
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'password',
+    password: 'password', 
     database: 'neepco_procurement_management',
     waitForConnections: true,
     connectionLimit: connectionLimit,
     queueLimit: 0
 });
+
+// user and password is device specific
 
 module.exports = {db};
