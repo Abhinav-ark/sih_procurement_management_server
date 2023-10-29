08a9a9e21d070531a79cd8899efdfc0e97baaab6
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2023-10-29 21:03:04 +0530

    sql update

--- connection.js
+++ connection.js
@@ -5,7 +5,7 @@ const connectionLimit = os.cpus().length;
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'sql003', 
+    password: 'password', 
     database: 'neepco_procurement_management',
     waitForConnections: true,
     connectionLimit: connectionLimit,
